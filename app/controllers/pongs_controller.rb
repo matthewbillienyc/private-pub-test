f05c9e2207b9ab72e9561636a9582a68e188b66f
chenghw <ho.won.cheng@gmail.com>
2015-11-20 09:37:53 -0500

    500 error fixed

--- app/controllers/pongs_controller.rb
+++ app/controllers/pongs_controller.rb
@@ -1,21 +1,26 @@
 class PongsController < ApplicationController
   def p1up
     PrivatePub.publish_to "/p1up", {}
+    render nothing: true
   end
 
   def p1down
     PrivatePub.publish_to "/p1down", {}
+    render nothing: true
   end
 
   def p2up
     PrivatePub.publish_to "/p2up", {}
+    render nothing: true
   end
 
   def p2down
     PrivatePub.publish_to "/p2down", {}
+    render nothing: true
   end
 
   def space
     PrivatePub.publish_to "/space", {}
+    render nothing: true
   end
 end
